1a3e1f217b58c144882cd58a19aecb90aa0862bc
Chang Zheng <changzheng300@foxmail.com>
2019-09-10 20:42:02 +0800

    添加了LQ326

---
+++ LeetCode题目集/326. Power of Three(easy)-循环除法.cpp
@@ -0,0 +1,20 @@
+/*
+ * @Autor: SourDumplings
+ * @Date: 2019-09-10 20:21:54
+ * @Link: https://github.com/SourDumplings/
+ * @Email: changzheng300@foxmail.com
+ * @Description: https://leetcode.com/problems/power-of-three/
+ */
+
+class Solution
+{
+public:
+    bool isPowerOfThree(int n)
+    {
+        while (n > 1 && n % 3 == 0)
+        {
+            n /= 3;
+        }
+        return n == 1;
+    }
+};
---
+++ LeetCode题目集/326. Power of Three(easy)-换底公式法2.java
@@ -0,0 +1,21 @@
+/*
+ * @Autor: SourDumplings
+ * @Date: 2019-09-10 20:32:57
+ * @Link: https://github.com/SourDumplings/
+ * @Email: changzheng300@foxmail.com
+ * @Description: https://leetcode.com/problems/power-of-three/
+ * 
+ * 设3^i = n，则i = log3(n) = log10(n) / log10(3)
+ * 判断这样得到的3^i还是不是n，或者i是不是整数即可（用 % 1判断）
+ */
+
+class Solution
+{
+    public boolean isPowerOfThree(int n)
+    {
+        if (n == 0)
+            return false;
+        int i = (int) (Math.log10(n) / Math.log10(3));
+        return Math.pow(3, i) == n;
+    }
+}
