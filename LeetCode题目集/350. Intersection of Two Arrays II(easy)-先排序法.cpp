bf491c28ebac3ba0c7ddb1979ec308519d8c880d
Chang Zheng <changzheng300@foxmail.com>
2019-09-14 10:34:45 +0800

    添加了LQ350

---
+++ LeetCode题目集/350. Intersection of Two Arrays II(easy)-先排序法.cpp
@@ -0,0 +1,34 @@
+/*
+ * @Autor: SourDumplings
+ * @Date: 2019-09-14 10:13:30
+ * @Link: https://github.com/SourDumplings/
+ * @Email: changzheng300@foxmail.com
+ * @Description: https://leetcode.com/problems/intersection-of-two-arrays-ii/
+ */
+
+class Solution
+{
+public:
+    vector<int> intersect(vector<int> &nums1, vector<int> &nums2)
+    {
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        vector<int> res;
+        int j = 0;
+        int n1 = nums1.size(), n2 = nums2.size();
+        for (int i = 0; i < n1; i++)
+        {
+            while (j < n2 && nums2[j] < nums1[i])
+                ++j;
+            if (j == n2)
+            {
+                break;
+            }
+            else if (nums2[j] == nums1[i])
+            {
+                res.push_back(nums2[j++]);
+            }
+        }
+        return res;
+    }
+};
---
+++ LeetCode题目集/350. Intersection of Two Arrays II(easy)-哈希表法.java
@@ -0,0 +1,64 @@
+import java.util.Hashtable;
+import java.util.Map;
+import java.util.Vector;
+
+/*
+ * @Autor: SourDumplings
+ * @Date: 2019-09-14 10:23:05
+ * @Link: https://github.com/SourDumplings/
+ * @Email: changzheng300@foxmail.com
+ * @Description: https://leetcode.com/problems/intersection-of-two-arrays-ii/
+ */
+
+class Solution
+{
+    public int[] intersect(int[] nums1, int[] nums2)
+    {
+        Map<Integer, Integer> map1 = new Hashtable<>();
+        Map<Integer, Integer> map2 = new Hashtable<>();
+        for (int i : nums1)
+        {
+            if (map1.containsKey(i))
+            {
+                map1.replace(i, map1.get(i) + 1);
+            }
+            else
+            {
+                map1.put(i, 1);
+            }
+        }
+        for (int i : nums2)
+        {
+            if (map2.containsKey(i))
+            {
+                map2.replace(i, map2.get(i) + 1);
+            }
+            else
+            {
+                map2.put(i, 1);
+            }
+        }
+        Vector<Integer> temp = new Vector<>();
+        for (int i : map1.keySet())
+        {
+            if (map2.containsKey(i))
+            {
+                int n1 = map1.get(i);
+                int n2 = map2.get(i);
+                int n = n1 < n2 ? n1 : n2;
+                while (n-- > 0)
+                {
+                    temp.add(i);
+                }
+            }
+        }
+        int n = temp.size();
+        int[] res = new int[n];
+        int i = 0;
+        for (int num : temp)
+        {
+            res[i++] = num;
+        }
+        return res;
+    }
+}
