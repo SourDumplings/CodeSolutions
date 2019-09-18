1abf9688b3342a5c4d0029139379cb796cc01ff2
Chang Zheng <changzheng300@foxmail.com>
2019-09-18 22:22:52 +0800

    添加了LQ383

---
+++ LeetCode题目集/383. Ransom Note(easy).cpp
@@ -0,0 +1,39 @@
+/*
+ * @Autor: SourDumplings
+ * @Date: 2019-09-18 22:12:03
+ * @Link: https://github.com/SourDumplings/
+ * @Email: changzheng300@foxmail.com
+ * @Description: https://leetcode.com/problems/ransom-note/
+ */
+
+class Solution
+{
+public:
+    bool canConstruct(string ransomNote, string magazine)
+    {
+        unordered_map<char, int> m;
+        for (auto &&c : magazine)
+        {
+            if (m.find(c) == m.end())
+            {
+                m[c] = 1;
+            }
+            else
+            {
+                ++m[c];
+            }
+        }
+        for (auto &&c : ransomNote)
+        {
+            if (m.find(c) == m.end() || m[c] == 0)
+            {
+                return false;
+            }
+            else
+            {
+                --m[c];
+            }
+        }
+        return true;
+    }
+};
---
+++ LeetCode题目集/383. Ransom Note(easy).java
@@ -0,0 +1,45 @@
+import java.util.Hashtable;
+import java.util.Map;
+
+/*
+ * @Autor: SourDumplings
+ * @Date: 2019-09-18 22:16:52
+ * @Link: https://github.com/SourDumplings/
+ * @Email: changzheng300@foxmail.com
+ * @Description: https://leetcode.com/problems/ransom-note/
+ */
+
+class Solution
+{
+    public boolean canConstruct(String ransomNote, String magazine)
+    {
+        Map<Character, Integer> map = new Hashtable<>();
+        int l1 = ransomNote.length();
+        int l2 = magazine.length();
+        for (int i = 0; i < l2; i++)
+        {
+            char c = magazine.charAt(i);
+            if (map.containsKey(c))
+            {
+                map.put(c, map.get(c) + 1);
+            }
+            else
+            {
+                map.put(c, 1);
+            }
+        }
+        for (int i = 0; i < l1; i++)
+        {
+            char c = ransomNote.charAt(i);
+            if (!map.containsKey(c) || map.get(c) == 0)
+            {
+                return false;
+            }
+            else
+            {
+                map.put(c, map.get(c) - 1);
+            }
+        }
+        return true;
+    }
+}
