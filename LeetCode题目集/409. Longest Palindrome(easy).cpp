e9a567d2594fc92bcb214dc77e16259a0f16060a
Chang Zheng <changzheng300@foxmail.com>
2019-09-23 16:00:55 +0800

    增加了LQ409

---
+++ LeetCode题目集/409. Longest Palindrome(easy).cpp
@@ -0,0 +1,45 @@
+/*
+ * @Autor: SourDumplings
+ * @Date: 2019-09-23 15:17:44
+ * @Link: https://github.com/SourDumplings/
+ * @Email: changzheng300@foxmail.com
+ * @Description: https://leetcode.com/problems/longest-palindrome/
+ * 
+ * 回文序列中奇数个数的元素只能最多有一个
+ */
+
+class Solution
+{
+public:
+    int longestPalindrome(string s)
+    {
+        int A[200];
+        fill(A, A + 200, 0);
+        for (auto &&c : s)
+        {
+            ++A[c - 'A'];
+        }
+        bool isFirstOdd = true;
+        int res = 0;
+        for (int i = 0; i < 200; i++)
+        {
+            if (A[i] & 1)
+            {
+                if (isFirstOdd)
+                {
+                    res += A[i];
+                    isFirstOdd = false;
+                }
+                else
+                {
+                    res += A[i] - 1;
+                }
+            }
+            else
+            {
+                res += A[i];
+            }
+        }
+        return res;
+    }
+};
---
+++ LeetCode题目集/409. Longest Palindrome(easy).java
@@ -0,0 +1,45 @@
+import java.util.Arrays;
+
+/*
+ * @Autor: SourDumplings
+ * @Date: 2019-09-23 15:55:05
+ * @Link: https://github.com/SourDumplings/
+ * @Email: changzheng300@foxmail.com
+ * @Description: https://leetcode.com/problems/longest-palindrome/
+ */
+
+class Solution
+{
+    public int longestPalindrome(String s)
+    {
+        int[] A = new int[200];
+        int l = s.length();
+        Arrays.fill(A, 0);
+        for (int i = 0; i < l; i++)
+        {
+            ++A[s.charAt(i) - 'A'];
+        }
+        int res = 0;
+        boolean isFirstOdd = true;
+        for (int i = 0; i < 200; i++)
+        {
+            if ((A[i] & 1) == 1)
+            {
+                if (isFirstOdd)
+                {
+                    res += A[i];
+                    isFirstOdd = false;
+                }
+                else
+                {
+                    res += A[i] - 1;
+                }
+            }
+            else
+            {
+                res += A[i];
+            }
+        }
+        return res;
+    }
+}
