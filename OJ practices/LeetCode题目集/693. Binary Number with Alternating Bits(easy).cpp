7528795880d4b3b2a158bb10bb079b4c8a1ee8eb
SourDumplings <changzheng300@foxmail.com>
2019-10-18 08:31:25 +0800

    刷LQ693

---
+++ OJ practices/LeetCode题目集/693. Binary Number with Alternating Bits(easy).cpp
@@ -0,0 +1,36 @@
+/*
+ * @Autor: SourDumplings
+ * @Date: 2019-10-18 07:56:41
+ * @Link: https://github.com/SourDumplings/
+ * @Email: changzheng300@foxmail.com
+ * @Description: https://leetcode.com/problems/binary-number-with-alternating-bits/
+ * 
+ * 注意对于int右移是用符号位补齐
+ */
+
+class Solution
+{
+public:
+    bool hasAlternatingBits(int n)
+    {
+        unsigned mask = 1 << 31;
+        int count = 32;
+        while ((mask & n) == 0)
+        {
+            mask >>= 1;
+            --count;
+        }
+        bool last = false;
+        while (count > 0)
+        {
+            if (last && (mask & n) != 0 || !last && (mask & n) == 0)
+            {
+                return false;
+            }
+            last = (mask & n) != 0;
+            mask >>= 1;
+            --count;
+        }
+        return true;
+    }
+};
---
+++ OJ practices/LeetCode题目集/693. Binary Number with Alternating Bits(easy).java
@@ -0,0 +1,33 @@
+/*
+ * @Autor: SourDumplings
+ * @Date: 2019-10-18 08:27:43
+ * @Link: https://github.com/SourDumplings/
+ * @Email: changzheng300@foxmail.com
+ * @Description: https://leetcode.com/problems/binary-number-with-alternating-bits/
+ */
+
+class Solution
+{
+    public boolean hasAlternatingBits(int n)
+    {
+        int mask = 1 << 31;
+        int count = 32;
+        while ((mask & n) == 0)
+        {
+            mask >>>= 1;
+            --count;
+        }
+        boolean last = false;
+        while (count > 0)
+        {
+            if (last && (mask & n) != 0 || !last && (mask & n) == 0)
+            {
+                return false;
+            }
+            last = (mask & n) != 0;
+            mask >>>= 1;
+            --count;
+        }
+        return true;
+    }
+}
