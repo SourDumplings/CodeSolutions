d24cf1a3e6a68bd1907a6e5599f56f1d82628711
SourDumplings <changzheng300@foxmail.com>
2019-10-24 10:37:04 +0800

    刷LQ704

---
+++ OJ practices/LeetCode题目集/704. Binary Search(easy)-二分查找1.cpp
@@ -0,0 +1,40 @@
+/*
+ * @Autor: SourDumplings
+ * @Date: 2019-10-24 10:24:37
+ * @Link: https://github.com/SourDumplings/
+ * @Email: changzheng300@foxmail.com
+ * @Description: https://leetcode.com/problems/binary-search/
+ */
+
+class Solution
+{
+public:
+    int search(vector<int> &nums, int target)
+    {
+        int lo = 0, hi = nums.size() - 1;
+        while (lo < hi)
+        {
+            int mi = (lo + hi) / 2;
+            if (nums[mi] == target)
+            {
+                return mi;
+            }
+            else if (nums[mi] < target)
+            {
+                lo = mi + 1;
+            }
+            else
+            {
+                hi = mi;
+            }
+        }
+        if (nums[lo] == target)
+        {
+            return lo;
+        }
+        else
+        {
+            return -1;
+        }
+    }
+};
---
+++ OJ practices/LeetCode题目集/704. Binary Search(easy)-二分查找2.java
@@ -0,0 +1,37 @@
+/*
+ * @Autor: SourDumplings
+ * @Date: 2019-10-24 10:28:18
+ * @Link: https://github.com/SourDumplings/
+ * @Email: changzheng300@foxmail.com
+ * @Description: https://leetcode.com/problems/binary-search/
+ * 
+ * 此为优化版本的二分查找，每次只需要一次比较
+ */
+
+class Solution
+{
+    public int search(int[] nums, int target)
+    {
+        int lo = 0, hi = nums.length;
+        while (lo < hi - 1)
+        {
+            int mi = (lo + hi) / 2;
+            if (target < nums[mi])
+            {
+                hi = mi;
+            }
+            else
+            {
+                lo = mi;
+            }
+        }
+        if (nums[lo] == target)
+        {
+            return lo;
+        }
+        else
+        {
+            return -1;
+        }
+    }
+}
