7ac6ff5aeadb2ba6a68245e1c07969346485d9b5
SourDumplings <changzheng300@foxmail.com>
2019-11-05 10:27:06 +0800

    刷LQ 748 754

---
+++ OJ practices/LeetCode题目集/748. Shortest Completing Word(easy).java
@@ -0,0 +1,60 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+ * @Autor: SourDumplings
+ * @Date: 2019-11-04 17:30:57
+ * @Link: https://github.com/SourDumplings/
+ * @Email: changzheng300@foxmail.com
+ * @Description: https://leetcode.com/problems/shortest-completing-word/
+ * 
+ * 其实对于字母作为键的不要用map，直接用数组比较好
+ */
+
+class Solution
+{
+    public String shortestCompletingWord(String licensePlate, String[] words)
+    {
+        Map<Character, Integer> map = new HashMap<>();
+        int l1 = licensePlate.length();
+        int l2 = words.length;
+        for (int i = 0; i < l1; i++)
+        {
+            Character c = licensePlate.charAt(i);
+            if (Character.isLetter(c))
+            {
+                map.merge(Character.toLowerCase(c), 1, Integer::sum);
+            }
+        }
+        int minL = Integer.MAX_VALUE;
+        String res = null;
+        for (int i = 0; i < l2; i++)
+        {
+            String word = words[i];
+            if (isOK(word, map) && word.length() < minL)
+            {
+                minL = word.length();
+                res = word;
+            }
+        }
+        return res;
+    }
+
+    private boolean isOK(final String word, Map<Character, Integer> map)
+    {
+        Map<Character, Integer> wordMap = new HashMap<>();
+        int l = word.length();
+        for (int i = 0; i < l; i++)
+        {
+            wordMap.merge(word.charAt(i), 1, Integer::sum);
+        }
+        for (Character c : map.keySet())
+        {
+            if (!wordMap.containsKey(c) || wordMap.get(c) < map.get(c))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ OJ practices/LeetCode题目集/754. Reach a Number(easy).java
@@ -0,0 +1,31 @@
+/*
+ * @Autor: SourDumplings
+ * @Date: 2019-11-05 09:56:06
+ * @Link: https://github.com/SourDumplings/
+ * @Email: changzheng300@foxmail.com
+ * @Description: https://leetcode.com/problems/reach-a-number/
+ * 
+ * 对于target正负是对称的，故先取绝对值
+ * 累加k，找到刚好使得和sum不小于target
+ * 如果sum == target那么结果就是k
+ * 如果sum > target，将二者作差得到d，必有d < k
+ * 如果d是偶数，那么在1~k中肯定能够找到一个值将其符号改为负即可满足sum == target
+ * 如果d是奇数，这个是不行的，因为无论改变1~k中谁的负号也没用，因为对于sum的减少一定是偶数的
+ * 那么需要再来一个数k + 1或者两个数把d搞成偶数才行
+ */
+
+class Solution
+{
+    public int reachNumber(int target)
+    {
+        target = Math.abs(target);
+        int k = 0;
+        int sum = 0;
+        while (sum < target)
+        {
+            sum += ++k;
+        }
+        int d = sum - target;
+        return d % 2 == 0 ? k : k + 1 + k % 2;
+    }
+}
