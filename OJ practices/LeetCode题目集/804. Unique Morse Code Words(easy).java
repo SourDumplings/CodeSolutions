059944a62db6fe6127e362858c65ae36d40026e2
SourDumplings <changzheng300@foxmail.com>
2019-11-19 19:11:49 +0800

    刷LQ 804，添加了Core Java的数据文件

---
+++ OJ practices/LeetCode题目集/804. Unique Morse Code Words(easy).java
@@ -0,0 +1,33 @@
+import java.util.HashSet;
+import java.util.Set;
+
+/*
+ * @Autor: SourDumplings
+ * @Date: 2019-11-19 19:03:16
+ * @Link: https://github.com/SourDumplings/
+ * @Email: changzheng300@foxmail.com
+ * @Description: https://leetcode.com/problems/unique-morse-code-words/
+ */
+
+class Solution
+{
+    public int uniqueMorseRepresentations(String[] words)
+    {
+        String[] mores =
+        { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---",
+                ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.." };
+        Set<String> set = new HashSet<>();
+        for (String word : words)
+        {
+            int l = word.length();
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < l; i++)
+            {
+                char c = word.charAt(i);
+                sb.append(mores[c - 'a']);
+            }
+            set.add(sb.toString());
+        }
+        return set.size();
+    }
+}
