3d8507c78e464e04966228955c0e5eb4260db947
SourDumplings <changzheng300@foxmail.com>
2020-01-06 20:06:26 +0800

    刷 LQ 5,6

---
+++ OJ practices/LeetCode题目集/6. ZigZag Conversion(medium)-按行排序.cpp
@@ -0,0 +1,50 @@
+/*
+ * @Autor: SourDumplings
+ * @Date: 2020-01-06 19:54:33
+ * @Link: https://github.com/SourDumplings/
+ * @Email: changzheng300@foxmail.com
+ * @Description: https://leetcode.com/problems/zigzag-conversion/
+ * 
+ * 因为最终结果是按行输出，其实只需要知道下一个字符该放在第几行即可，即不用关心列
+ */
+
+class Solution
+{
+public:
+    string convert(string s, int numRows)
+    {
+        if (numRows == 1)
+        {
+            return s;
+        }
+
+        vector<string> a(numRows);
+        int r = 0;
+        bool moveDown = false;
+        for (char c : s)
+        {
+            a[r] += c;
+            if (r == numRows - 1 || r == 0)
+            {
+                moveDown = !moveDown;
+            }
+            if (moveDown)
+            {
+                ++r;
+            }
+            else
+            {
+                --r;
+            }
+        }
+        string ret;
+        for (auto &&str : a)
+        {
+            for (auto &&c : str)
+            {
+                ret += c;
+            }
+        }
+        return ret;
+    }
+};
---
+++ OJ practices/LeetCode题目集/6. ZigZag Conversion(medium)-蛮力算法.java
@@ -0,0 +1,73 @@
+/*
+ * @Autor: SourDumplings
+ * @Date: 2020-01-06 18:59:14
+ * @Link: https://github.com/SourDumplings/
+ * @Email: changzheng300@foxmail.com
+ * @Description: https://leetcode.com/problems/zigzag-conversion/
+ */
+
+class Solution
+{
+    public String convert(String s, int numRows)
+    {
+        if (numRows == 1)
+        {
+            return s;
+        }
+        int numCols = 500;
+        Character[][] a = new Character[numRows][numCols];
+        int l = s.length();
+        int r = 0, c = 0;
+        boolean downMove = true;
+        for (int i = 0; i < l; i++)
+        {
+            Character ch = s.charAt(i);
+            a[r][c] = ch;
+            if (downMove)
+            {
+                if (r == numRows - 1)
+                {
+                    ++c;
+                    --r;
+                    downMove = false;
+                }
+                else
+                {
+                    ++r;
+                }
+            }
+            else
+            {
+                if (r == 0)
+                {
+                    downMove = true;
+                    ++r;
+                }
+                else
+                {
+                    --r;
+                    ++c;
+                }
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        int count = 0;
+        for (int i = 0; i < numRows; i++)
+        {
+            for (int j = 0; j < numCols; j++)
+            {
+                Character ch = a[i][j];
+                if (ch != null)
+                {
+                    sb.append(ch);
+                    ++count;
+                }
+                if (count == l)
+                {
+                    return sb.toString();
+                }
+            }
+        }
+        return null;
+    }
+}
