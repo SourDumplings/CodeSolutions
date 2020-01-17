fc97d088b777deefde94f1512c1edc05b7e7142f
SourDumplings <changzheng300@foxmail.com>
2020-01-17 11:23:42 +0800

    刷 LQ 17

---
+++ OJ practices/LeetCode题目集/17. Letter Combinations of a Phone Number(medium)-BFS.java
@@ -0,0 +1,62 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/*
+ * @Autor: SourDumplings
+ * @Date: 2020-01-17 10:03:14
+ * @Link: https://github.com/SourDumplings/
+ * @Email: changzheng300@foxmail.com
+ * @Description: https://leetcode.com/problems/letter-combinations-of-a-phone-number/
+ */
+
+class Solution
+{
+    public List<String> letterCombinations(String digits)
+    {
+        String[] A = new String[]
+        { null, null, "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
+        int l = digits.length();
+        Queue<StringBuilder> temp = new LinkedList<>();
+        boolean first = true;
+        for (int i = 0; i < l; i++)
+        {
+            int d = digits.charAt(i) - '0';
+            String letters = A[d];
+            int ll = letters.length();
+            StringBuilder sb = null;
+            if (first)
+            {
+                first = false;
+                sb = new StringBuilder();
+                for (int j = 0; j < ll; j++)
+                {
+                    char c = letters.charAt(j);
+                    temp.add(new StringBuilder(sb).append(c));
+                }
+            }
+            else
+            {
+                Queue<StringBuilder> tempQ = new LinkedList<>();
+                while (!temp.isEmpty())
+                {
+                    sb = temp.poll();
+                    for (int j = 0; j < ll; j++)
+                    {
+                        char c = letters.charAt(j);
+                        tempQ.add(new StringBuilder(sb).append(c));
+                    }
+                }
+                temp = tempQ;
+            }
+
+        }
+        List<String> res = new ArrayList<>();
+        for (StringBuilder sb : temp)
+        {
+            res.add(sb.toString());
+        }
+        return res;
+    }
+}
---
+++ OJ practices/LeetCode题目集/17. Letter Combinations of a Phone Number(medium)-DFS.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * @Autor: SourDumplings
+ * @Date: 2020-01-17 11:08:40
+ * @Link: https://github.com/SourDumplings/
+ * @Email: changzheng300@foxmail.com
+ * @Description: https://leetcode.com/problems/letter-combinations-of-a-phone-number/
+ */
+
+class Solution
+{
+    private String[] A = new String[]
+    { null, null, "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
+
+    public List<String> letterCombinations(String digits)
+    {
+        List<String> res = new ArrayList<>();
+
+        if (!digits.isEmpty())
+        {
+            DFS(new StringBuilder(), digits, res);
+        }
+        return res;
+    }
+
+    private void DFS(StringBuilder sb, String digits, List<String> res)
+    {
+        if (digits.isEmpty())
+        {
+            res.add(sb.toString());
+        }
+        else
+        {
+            int d = digits.charAt(0) - '0';
+            String letters = A[d];
+            int l = letters.length();
+            for (int i = 0; i < l; i++)
+            {
+                char c = letters.charAt(i);
+                DFS(new StringBuilder(sb).append(c), digits.substring(1), res);
+            }
+        }
+    }
+}
