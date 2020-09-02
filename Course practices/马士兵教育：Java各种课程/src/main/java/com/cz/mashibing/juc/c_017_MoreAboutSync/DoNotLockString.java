309436ead7578a962f160c8fae713d990ac5cb44
SourDumplings <changzheng300@foxmail.com>
2020-09-02 10:30:01 +0800

    添加了 synchronized 的一些其他内容.

--- Course practices/马士兵教育：Java各种课程/src/main/java/com/cz/mashibing/juc/c_017_MoreAboutSync/DoNotLockString.java
+++ Course practices/马士兵教育：Java各种课程/src/main/java/com/cz/mashibing/juc/c_017_MoreAboutSync/DoNotLockString.java
@@ -1,33 +1,67 @@
 /**
- * ��Ҫ���ַ���������Ϊ��������
- * ������������У�m1��m2��ʵ��������ͬһ������
- * ����������ᷢ���ȽϹ�������󣬱������õ���һ����⣬�ڸ�����д����������ַ�����Hello����
- * �����������Դ�룬���������Լ��Ĵ�����Ҳ������"Hello",��ʱ����п��ܷ����ǳ����������������
- * ��Ϊ��ĳ�������õ�����ⲻ�����ʹ����ͬһ����
- * 
+ * 不要以字符串常量作为锁定对象
+ * 在下面的例子中，m1和m2其实锁定的是同一个对象
+ * 这种情况还会发生比较诡异的现象，比如你用到了一个类库，在该类库中代码锁定了字符串“Hello”，
+ * 但是你读不到源码，所以你在自己的代码中也锁定了"Hello",这时候就有可能发生非常诡异的死锁阻塞，
+ * 因为你的程序和你用到的类库不经意间使用了同一把锁
+ * <p>
  * jetty
- * 
+ *
  * @author mashibing
  */
 package com.cz.mashibing.juc.c_017_MoreAboutSync;
 
-public class DoNotLockString {
-	
-	String s1 = "Hello";
-	String s2 = "Hello";
+import java.util.concurrent.TimeUnit;
 
-	void m1() {
-		synchronized(s1) {
-			
-		}
-	}
-	
-	void m2() {
-		synchronized(s2) {
-			
-		}
-	}
+public class DoNotLockString
+{
 
-	
+    static String s1 = "Hello";
+    static String s2 = "Hello";
+    // static String s2 = "World";
+
+    static void m1()
+    {
+        synchronized (s1)
+        {
+            while (true)
+            {
+                try
+                {
+                    TimeUnit.SECONDS.sleep(1);
+                }
+                catch (InterruptedException e)
+                {
+                    e.printStackTrace();
+                }
+                System.out.println(Thread.currentThread().getName());
+            }
+        }
+    }
+
+    static void m2()
+    {
+        synchronized (s2)
+        {
+            while (true)
+            {
+                try
+                {
+                    TimeUnit.SECONDS.sleep(1);
+                }
+                catch (InterruptedException e)
+                {
+                    e.printStackTrace();
+                }
+                System.out.println(Thread.currentThread().getName());
+            }
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        new Thread(DoNotLockString::m1, "t1").start();
+        new Thread(DoNotLockString::m2, "t2").start();
+    }
 
 }
--- Course practices/马士兵教育：Java各种课程/src/main/java/com/cz/mashibing/juc/c_017_MoreAboutSync/SyncSameObject.java
+++ Course practices/马士兵教育：Java各种课程/src/main/java/com/cz/mashibing/juc/c_017_MoreAboutSync/SyncSameObject.java
@@ -1,7 +1,9 @@
+
 /**
- * ����ĳ����o�����o�����Է����ı䣬��Ӱ������ʹ��
- * �������o�������һ�������������Ķ������ı�
- * Ӧ�ñ��⽫������������ñ������Ķ���
+ * 锁定某对象o，如果o的属性发生改变，不影响锁的使用
+ * 但是如果o变成另外一个对象，则锁定的对象发生改变
+ * 应该避免将锁定对象的引用变成另外的对象
+ *
  * @author mashibing
  */
 package com.cz.mashibing.juc.c_017_MoreAboutSync;
@@ -9,44 +11,54 @@ package com.cz.mashibing.juc.c_017_MoreAboutSync;
 import java.util.concurrent.TimeUnit;
 
 
-public class SyncSameObject {
-	
-	/*final*/ Object o = new Object();
+public class SyncSameObject
+{
 
-	void m() {
-		synchronized(o) {
-			while(true) {
-				try {
-					TimeUnit.SECONDS.sleep(1);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-				System.out.println(Thread.currentThread().getName());
-				
-				
-			}
-		}
-	}
-	
-	public static void main(String[] args) {
-		SyncSameObject t = new SyncSameObject();
-		//������һ���߳�
-		new Thread(t::m, "t1").start();
-		
-		try {
-			TimeUnit.SECONDS.sleep(3);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		//�����ڶ����߳�
-		Thread t2 = new Thread(t::m, "t2");
-		
-		t.o = new Object(); //���������ı䣬����t2�̵߳���ִ�У����ע�͵���仰���߳�2����Զ�ò���ִ�л���
-		
-		t2.start();
-		
-	}
+    /*final*/ Object o = new Object();
 
-	
+    void m()
+    {
+        synchronized (o)
+        {
+            while (true)
+            {
+                try
+                {
+                    TimeUnit.SECONDS.sleep(1);
+                }
+                catch (InterruptedException e)
+                {
+                    e.printStackTrace();
+                }
+                System.out.println(Thread.currentThread().getName());
+
+
+            }
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        SyncSameObject t = new SyncSameObject();
+        //启动第一个线程
+        new Thread(t::m, "t1").start();
+
+        try
+        {
+            TimeUnit.SECONDS.sleep(3);
+        }
+        catch (InterruptedException e)
+        {
+            e.printStackTrace();
+        }
+        //创建第二个线程
+        Thread t2 = new Thread(t::m, "t2");
+
+        // t.o = new Object(); //锁对象发生改变，所以t2线程得以执行，如果注释掉这句话，线程2将永远得不到执行机会
+        // 因此为了防止锁的对象被改变，则通常需要加 final
+        t2.start();
+
+    }
+
 
 }
