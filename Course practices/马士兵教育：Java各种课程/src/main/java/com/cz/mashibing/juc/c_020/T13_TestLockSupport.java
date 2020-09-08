a13341130a1e483b8bce16b40be82f89faffaec8
SourDumplings <changzheng300@foxmail.com>
2020-09-08 17:59:57 +0800

    添加了 LockSupport 的内容.

--- Course practices/马士兵教育：Java各种课程/src/main/java/com/cz/mashibing/juc/c_020/T13_TestLockSupport.java
+++ Course practices/马士兵教育：Java各种课程/src/main/java/com/cz/mashibing/juc/c_020/T13_TestLockSupport.java
@@ -12,8 +12,10 @@ public class T13_TestLockSupport
             for (int i = 0; i < 10; i++)
             {
                 System.out.println(i);
-                if (i == 5)
+                // if (i == 5)
+                if (i == 5 || i == 7)
                 {
+                    // 当前线程停止
                     LockSupport.park();
                 }
                 try
@@ -29,16 +31,25 @@ public class T13_TestLockSupport
 
         t.start();
 
-        LockSupport.unpark(t);
-
-        /*try {
+/*        try
+        {
             TimeUnit.SECONDS.sleep(8);
-        } catch (InterruptedException e) {
+        }
+        catch (InterruptedException e)
+        {
             e.printStackTrace();
         }
-        System.out.println("after 8 senconds!");
+        System.out.println("after 8 senconds!");*/
 
-        LockSupport.unpark(t);*/
+        LockSupport.unpark(t); // 让线程继续
+
+        /* LoclSupport 提供了对线程的控制功能，
+        * 是为了实现锁而定的
+        * 如果没有等 8 秒的代码那么 t 线程不会停了
+        * 即 unpark 可以在 park 之前调用，表示提前放行一次
+        * 但是如果要停两次，即 i == 5 || i == 7 的时候停
+        * 这样 unpark 还是无法阻止停在 i == 7 的时候，因为其本质是维护了一个计数器
+        * 故比使用锁更灵活 */
 
     }
 }
