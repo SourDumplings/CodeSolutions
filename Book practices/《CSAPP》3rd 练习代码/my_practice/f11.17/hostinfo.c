0b56fda1524476d8e033007d755bcfbd2b3df7d2
SourDumplings <changzheng300@foxmail.com>
2024-02-05 12:00:57 +0800

    添加了 CSAPP F11.17 代码

---
+++ Book practices/《CSAPP》3rd 练习代码/my_practice/f11.17/hostinfo.c
@@ -0,0 +1,49 @@
+/**
+ * @file hostinfo.c
+ * @author  ()
+ * @brief 根据域名获取 host 信息，类似于 nslookup 命令
+ * @version 1.0.0
+ * @date 2024-02-05
+ * 
+ * @copyright Copyright (c) 2024 SourDumplings
+ * 
+ * 编译：gcc -g -Wall hostinfo.c ../csapp.c -o hostinfo -lpthread
+ */
+
+#include "../csapp.h"
+
+int main(int argc, char **argv)
+{
+    struct addrinfo *p, *listp, hints;
+    char buf[MAXLINE];
+    int rc, flags;
+
+    if (argc != 2)
+    {
+        fprintf(stderr, "usage: %s <domain name>\n", argv[0]);
+        exit(0);
+    }
+
+    /* Get a list of addrinfo records */
+    memset(&hints, 0, sizeof(struct addrinfo));
+    hints.ai_family = AF_INET;       /* IPv4 only */        //line:netp:hostinfo:family
+    hints.ai_socktype = SOCK_STREAM; /* Connections only */ //line:netp:hostinfo:socktype
+    if ((rc = getaddrinfo(argv[1], NULL, &hints, &listp)) != 0)
+    {
+        fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(rc));
+        exit(1);
+    }
+
+    /* Walk the list and display each IP address */
+    flags = NI_NUMERICHOST; /* Display address string instead of domain name */
+    for (p = listp; p; p = p->ai_next)
+    {
+        Getnameinfo(p->ai_addr, p->ai_addrlen, buf, MAXLINE, NULL, 0, flags);
+        printf("%s\n", buf);
+    }
+
+    /* Clean up */
+    Freeaddrinfo(listp);
+
+    exit(0);
+}
