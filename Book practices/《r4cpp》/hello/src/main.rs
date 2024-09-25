6781234a289c2aed51fdab714bcd9f3fe4b6ae58
SourDumplings <changzheng300@foxmail.com>
2024-09-25 09:05:09 +0800

    define foo

--- Book practices/《r4cpp》/hello/src/main.rs
+++ Book practices/《r4cpp》/hello/src/main.rs
@@ -1,5 +1,11 @@
+fn foo(_x: &'static str) -> &'static str {
+    // "World No Return"
+    return "FooWorld";
+}
+
+
 fn main() {
     // let world = "World";
-    let world: &'static str = "World";
-    println!("Hello, {}!", world);
+    // let world: &'static str = "World";
+    println!("Hello, {}!", foo("bar"));
 }
