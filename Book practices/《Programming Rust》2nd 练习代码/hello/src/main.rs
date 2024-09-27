06b98a021af533e2f185e7d3ba166f6acbc5c9f7
SourDumplings <changzheng300@foxmail.com>
2024-09-27 08:59:24 +0800

    test assert

--- Book practices/《Programming Rust》2nd 练习代码/hello/src/main.rs
+++ Book practices/《Programming Rust》2nd 练习代码/hello/src/main.rs
@@ -1,6 +1,7 @@
 fn gcd(mut n: u64, mut m: u64) -> u64
 {
-    assert!(n != 0 && m != 0);
+    // assert!(n != 0 && m != 0);
+    debug_assert!(n != 0 && m != 0); // cargo run --release 下不会检查
     while m != 0
     {
         if m < n
@@ -17,5 +18,6 @@ fn gcd(mut n: u64, mut m: u64) -> u64
 fn main()
 {
     let res = gcd(15, 25);
+    // let res = gcd(0, 25);
     println!("gcd of 15 and 25 is {}", res);
 }
