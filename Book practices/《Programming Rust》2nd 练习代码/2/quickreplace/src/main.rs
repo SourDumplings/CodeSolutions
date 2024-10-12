8b02c6c198578c52a46d2e13c57cdd8dbd00c6d5
SourDumplings <sourdumplings@qq.com>
2024-10-12 08:03:04 +0800

    文件读写

--- Book practices/《Programming Rust》2nd 练习代码/2/quickreplace/src/main.rs
+++ Book practices/《Programming Rust》2nd 练习代码/2/quickreplace/src/main.rs
@@ -2,7 +2,7 @@
 //
 // File Description: 快速替换文件内容
 
-use std::env;
+use std::{env, f32::consts::E, fs};
 use text_colorizer::*;
 
 #[derive(Debug)]
@@ -51,5 +51,35 @@ fn parse_args() -> Arguments
 fn main()
 {
     let args = parse_args();
-    println!("{:?}", args);
+
+    let data = match fs::read_to_string(&args.filename)
+    {
+        Ok(v) => v,
+        Err(e) =>
+        {
+            eprintln!(
+                "{} failed to read from file '{}' : {:?}",
+                "Error:".red().bold(),
+                args.filename,
+                e
+            );
+            std::process::exit(1);
+        }
+    };
+
+    match fs::write(&args.output, &data)
+    {
+        Ok(_) =>
+        {}
+        Err(e) =>
+        {
+            eprintln!(
+                "{} failed to write to file '{}' : {:?}",
+                "Error:".red().bold(),
+                args.filename,
+                e
+            );
+            std::process::exit(1);
+        }
+    };
 }
